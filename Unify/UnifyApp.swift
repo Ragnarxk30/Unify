53b2806ddcc13ac89c67cab270413d114b5eda9f
Luke <fortniteragnaroek@gmail.com>
2025-10-27 14:33:17 +0100

    Initial Commit

---
+++ Unify/UnifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnifyApp.swift
+//  Unify
+//
+//  Created by Ragnarök on 27.10.25.
+//
+
+import SwiftUI
+
+@main
+struct UnifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
