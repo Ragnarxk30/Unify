f32dbb0f88c1e70f8ed87e8fd403765077977eae
jonas <jonas_dunkenberger@web.de>
2025-11-04 16:42:22 +0100

    First Endpoint for Groups, and User ID with auth.session

---
+++ Backend/Endpoints/GroupRepository.swift
@@ -0,0 +1,22 @@
+//
+//  GroupRepository.swift
+//  Unify
+//
+//  Created by Jonas Dunkenberger on 04.11.25.
+//
+
+
+import Foundation
+
+//Blueprint für die Group-Endpoints
+protocol GroupRepository {
+    func create(name: String, owner_id: UUID) async throws
+    //func rename(groupId: UUID, to newName: String) async throws
+    //func delete(groupId: UUID) async throws
+
+    //func groupsOwnedBy(userId: UUID, limit: Int, offset: Int) async throws -> [Group]
+    //func groupsForMember(userId: UUID, limit: Int, offset: Int) async throws -> [Group]
+
+    //func addMember(groupId: UUID, userId: UUID, role: String) async throws
+    //func removeMember(groupId: UUID, userId: UUID) async throws
+}
---
+++ Backend/Endpoints/group_endpoints.swift
@@ -0,0 +1,76 @@
+//
+//  group_endpoints.swift
+//  Unify
+//
+//  Created by Jonas Dunkenberger on 04.11.25.
+//
+
+import Foundation
+import Supabase
+
+struct SupabaseGroupRepository: GroupRepository {
+    
+    
+    private let db = supabase
+    private let groupsTable = "group"
+    private let membersTable = "group-members"
+    private let auth: AuthRepository
+
+    init(auth: AuthRepository = SupabaseAuthRepository()) {
+        self.auth = auth
+    }
+    
+    // Encodable Payloads
+    private struct CreateGroupPayload: Encodable {
+        let name: String
+        let ownerId: UUID
+        // <- Case-Name == Property-Name; Mapping passiert rechts
+        enum CodingKeys: String, CodingKey {
+            case name
+            case ownerId = "owner_id"
+        }
+    }
+
+    // MARK: - CRUD
+    func create(name: String, owner_id: UUID) async throws {
+        // auskommentiert bis wir uns richtig einloggen können
+        //let ownerId = try await auth.currentUserId()
+        let ownerId = owner_id
+        
+        // <- Richtige Label verwenden
+        let payload = CreateGroupPayload(name: name, ownerId: ownerId)
+
+        try await db
+            .from(groupsTable)
+            .insert(payload)
+            .execute()
+
+    }
+
+    func rename(groupId: UUID, to newName: String) async throws {
+        
+    }
+    
+    
+    func delete(groupId: UUID) async throws {
+        
+    }
+    
+    //func groupsOwnedBy(userId: UUID, limit: Int, offset: Int) async throws -> [Group] {
+        
+    //}
+    
+    //func groupsForMember(userId: UUID, limit: Int, offset: Int) async throws -> [Group] {
+        
+    //}
+    
+    func addMember(groupId: UUID, userId: UUID, role: String) async throws {
+        
+    }
+    
+    func removeMember(groupId: UUID, userId: UUID) async throws {
+        
+    }
+    
+    // … deine restlichen Methoden bleiben wie sie sind …
+}
